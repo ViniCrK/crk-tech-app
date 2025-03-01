4af8d3e090c1b2a1b71d3339056e025c19f79742
ViniCrK <vinicrk@outlook.com>
2025-03-01 11:32:38 -0300

    Renomear arquivo para melhor organização

--- src/data/InicioData.tsx
+++ src/data/InicioData.tsx
@@ -1,25 +1,25 @@
 import { ImageSourcePropType } from "react-native";
 
-export type ImagemCarrosselType = {
-  titulo: string;
+export type ImagensInicioProps = {
   imagem: ImageSourcePropType;
+  titulo: string;
   descricao: string;
 };
 
-export const CarrosselImagens = [
+export const ImagensInicio = [
   {
-    titulo: "imagem 1",
     imagem: require("@/assets/images/pc-2.jpg"),
+    titulo: "imagem 1",
     descricao: "descricao imagem 1",
   },
   {
-    titulo: "imagem 2",
     imagem: require("@/assets/images/pc-2.jpg"),
+    titulo: "imagem 2",
     descricao: "descricao imagem 2",
   },
   {
-    titulo: "imagem 3",
     imagem: require("@/assets/images/pc-2.jpg"),
+    titulo: "imagem 3",
     descricao: "descricao imagem 3",
   },
 ];
