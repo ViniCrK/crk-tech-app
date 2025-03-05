bec78498796aebac281a4a91c3f8dddfcf711ca2
ViniCrK <vinicrk@outlook.com>
2025-03-05 13:57:46 -0300

    Adicionar novos ícones para os serviços no arquivo ServicosData.tsx

--- src/data/ServicosData.tsx
+++ src/data/ServicosData.tsx
@@ -8,31 +8,31 @@ export type CardServicosType = {
 
 export const CardServicosData = [
   {
-    icone: require("@/assets/images/pc-2.jpg"),
+    icone: require("@/assets/icones/computador.png"),
     titulo: "Montagem de Computadores",
     descricao:
       "Faremos a montagem do seu computador a partir das peças à sua escolha.",
   },
   {
-    icone: require("@/assets/images/pc-2.jpg"),
+    icone: require("@/assets/icones/limpeza-computador.png"),
     titulo: "Limpeza de Computadores",
     descricao:
       "Faremos uma limpeza no seu computador por completo para que pareça como se estivesse acabado de sair da caixa.",
   },
   {
-    icone: require("@/assets/images/pc-2.jpg"),
+    icone: require("@/assets/icones/limpeza-notebook.png"),
     titulo: "Limpeza de Notebooks",
     descricao:
       "Faremos uma limpeza profunda e completa no seu notebook para que ele dure uma vida inteira.",
   },
   {
-    icone: require("@/assets/images/pc-2.jpg"),
+    icone: require("@/assets/icones/upgrade.png"),
     titulo: "Upgrade de peças",
     descricao:
       "Faremos um upgrade na peça desejada em seu Computador ou Notebook.",
   },
   {
-    icone: require("@/assets/images/pc-2.jpg"),
+    icone: require("@/assets/icones/consultoria.png"),
     titulo: "Consultoria Especializada",
     descricao:
       "Escolheremos juntos as melhores peças para suprirem a sua necessidade.",
