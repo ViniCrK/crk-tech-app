fb164c463f6e35d6560e912f0f4b78b7077623b0
ViniCrK <vinicrk@outlook.com>
2025-04-19 22:54:59 -0300

    Configurar conexão com o firebase

---
+++ firebase-config.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { app, auth };
