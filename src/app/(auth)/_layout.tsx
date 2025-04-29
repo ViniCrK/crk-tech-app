fc3f39e0ab12d7f84109f487829d09cd57154324
ViniCrK <vinicrk@outlook.com>
2025-04-29 18:42:19 -0300

    Refatorar a autenticação do layout e adicionar a inicialização do Firestore

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,13 +1,22 @@
+import { useEffect, useState } from "react";
 import { Redirect, Stack } from "expo-router";
 import { auth } from "@/config/firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
 
 export default function AuthLayout() {
-  onAuthStateChanged(auth, (usuario) => {
-    if (usuario == null) {
-      return <Redirect href="/login" />;
-    }
-  });
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (usuario) => {
+      setUser(usuario);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (user === null) {
+    return <Redirect href="/login" />;
+  }
 
   return (
     <Stack>
