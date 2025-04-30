768ebd6820e88fd8d7efc673f528fbfb2d970218
ViniCrK <vinicrk@outlook.com>
2025-04-30 19:31:43 -0300

    Consertar o comportamento dos formulários de solicitação e cadastro do serviço

--- src/app/(auth)/(tabs)/servicos/_layout.tsx
+++ src/app/(auth)/(tabs)/servicos/_layout.tsx
@@ -15,6 +15,10 @@ export default function ServicosLayout() {
           name="solicitar"
           options={{ title: "Solicitar Serviço" }}
         />
+        <Stack.Screen
+          name="cadastrar"
+          options={{ title: "Cadastrar Serviço" }}
+        />
       </Stack>
     </>
   );
--- src/app/(auth)/(tabs)/servicos/index.tsx
+++ src/app/(auth)/(tabs)/servicos/index.tsx
@@ -1,11 +1,37 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import ListaServicos from "./components/ListaServicos";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/config/firebase-config";
+import { Link } from "expo-router";
 
 export default function Servicos() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (usuario) => {
+      if (usuario) {
+        if (usuario.email === "vinicius260803@gmail.com") {
+          setIsAdmin(true);
+        }
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.titulo}>NOSSOS SERVIÇOS</Text>
+
+      {isAdmin && (
+        <Link href="/servicos/cadastrar" asChild>
+          <TouchableOpacity style={styles.botaoAdd}>
+            <Text style={styles.textoBotaoAdd}>+</Text>
+          </TouchableOpacity>
+        </Link>
+      )}
 
       <ListaServicos />
     </SafeAreaView>
@@ -26,5 +52,21 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     textAlign: "left",
+  },
+  botaoAdd: {
+    position: "absolute",
+    top: 60,
+    right: 40,
+    backgroundColor: "#FFF",
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textoBotaoAdd: {
+    color: "#2547A0",
+    fontSize: 36,
+    lineHeight: 46,
   },
 });
